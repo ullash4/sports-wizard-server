174b3465546f0101493f9b03d39e8a08adb1e66a
ullash4 <programmerullash@gmail.com>
2022-05-07 21:54:14 +0600

    implement delete

--- index.js
+++ index.js
@@ -47,6 +47,14 @@ async function run() {
             res.send(result);
         })
 
+        // Delete item
+        app.delete("/product/:id", async(req, res)=>{
+            const id  = req.params.id;
+            const query = {_id: ObjectId(id)}
+            const result = await productCollection.deleteOne(query)
+            res.send(result);
+        })
+
     } finally {
         // await client.close();
     }
