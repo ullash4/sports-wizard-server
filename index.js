0ce04b6b735733905dbcdbe210d6351c6c60ea9a
ullash4 <programmerullash@gmail.com>
2022-05-16 20:10:20 +0600

    hlw there added

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(cors())
 app.use(express.json())
 
 
-
+//hlw threre
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1hpxp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
